952f295e9e525d04539de2e0d0e199a92dc09557
milkmidi <milkmidi@gmail.com>
2017-07-06 16:05:27 +0800

    drop

---
+++ src/DropJack/index.js
@@ -0,0 +1,21 @@
+import React , { Component } from 'react';
+import './index.css';
+import logic from './logic';
+class DropJack extends Component{
+
+  constructor(props){
+    super(props);
+  }
+  componentDidMount() {
+    logic.init();
+  }
+  render(){
+    return (
+      <div className="drop-jack-root">
+        <div id="canvas_wrap"></div>
+      </div>
+    )
+  }
+}
+
+export default DropJack;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
+import DropJack from './DropJack';
 import './index.css';
 import './index.html';
 
-render(<App />, document.querySelector('#root'));
+render(
+  <div>
+    <App />
+    <DropJack />
+  </div>,
+  document.querySelector('#root'));

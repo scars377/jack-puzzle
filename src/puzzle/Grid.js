cc280d2ee005c4e8a126fcc3a3c99d322cd91de1
arthur <arthur@17.media>
2017-07-07 13:55:01 +0800

    feature: add click for puzzle

--- src/puzzle/Grid.js
+++ src/puzzle/Grid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Grid = ({pos, id, visible, image})=>{
+const Grid = ({pos, id, visible, image, handleClick})=>{
   const x = pos%3;
   const y = parseInt(pos/3, 10);
   const u = id%3;
@@ -9,7 +9,9 @@ const Grid = ({pos, id, visible, image})=>{
     <div className='grid' style={{
       transform: `translate(${x*100}px, ${y*100}px)`,
       display: visible?'block':'none',
-    }}>
+    }}
+      onClick={handleClick}
+    >
       <div className='img' style={{
         transform: `translate(-${u*100}px, -${v*100}px)`,
         backgroundImage: `url('${image}')`,
